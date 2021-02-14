b255ad579ffd182604e50c0e0cdb0b41187fa656
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2021-02-15 00:59:58 +0700

    update Login page

--- react-two-factor-authentication/src/App.js
+++ react-two-factor-authentication/src/App.js
@@ -1,15 +1,28 @@
-import Route from './components/Route'
+import React from 'react'
+import {Route, BrowserRouter} from 'react-router-dom';
+import {ProtectedRoute} from './components/ProtectedRoute/ProtectedRoute'
 import logo from './logo.svg';
 import './App.scss';
 
 import Login from './views/Login'
+import Main from './views/Main'
 
-function App() {
-  return (
-    <div className="App">
-      <Route path="/" ><Login/></Route>
-    </div>
-  );
+class App extends  React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Route path="/login"  component={Login} />
+          <ProtectedRoute path="/" component={Main} />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-two-factor-authentication/src/views/Login/Login.jsx
+++ react-two-factor-authentication/src/views/Login/Login.jsx
@@ -1,14 +1,41 @@
 import React, { PureComponent } from 'react';
+import auth from '../../services/AuthService/AuthService'
 import PropTypes from 'prop-types';
 import  './Login.scss';
 
 class Login extends PureComponent { 
   constructor(props) {
     super(props);
-
     this.state = {
-      hasError: false,
-    };
+      username : '', 
+      password : ''
+    }
+    this.onLogin = this.onLogin.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onLogin(event) {
+    event.preventDefault();
+    const { username, password } = this.state;
+    let usernamePassword = username + ':' + password;
+    let usernamePasswordBase64 = btoa(usernamePassword);
+    
+    auth.login(usernamePasswordBase64)
+      .then((response) => {
+        console.log(response);
+        auth.setAuthenticated(true);
+        this.props.history.push("/")
+      })
+      .catch((error) => {
+        if(error.response) {
+          console.log(error.response.status);
+        }
+      });
+  }
+
+  onChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name] : value });
   }
 
   componentWillMount = () => {
@@ -36,9 +63,7 @@ class Login extends PureComponent {
   }
 
   render () {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
-    }
+    const { username, password } = this.state;
     return (
       <div className="container">
          <div className="form-container">
@@ -46,13 +71,27 @@ class Login extends PureComponent {
             <h2 className="title">Sign in</h2>
             <div className="input-field">
               <i className="fa fa-user"></i>
-              <input type="text" placeholder="username"/>
+              <input 
+                type="text"
+                name="username"
+                value={username} 
+                placeholder="username"
+                onChange={this.onChange} />
             </div>
             <div className="input-field">
               <i className="fa fa-lock"></i>
-              <input type="text" placeholder="password"/>
+              <input 
+                type="text" 
+                name="password"
+                value={password} 
+                placeholder="password"
+                onChange={this.onChange}/>
             </div>
-            <input className="btn" type="submit" value="Login"/>
+            <input 
+              type="submit" 
+              className="btn" 
+              value="Login" 
+              onClick={this.onLogin} />
             <p className="social-text">Or Sign in with social platforms</p>
             <div className="social-media">
               <a href="#" className="social-icon"><i className="fa fa-github"></i></a>
